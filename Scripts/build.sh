791b3b3a3097c0ff8a4a96c135dbd6d381c85285
Haeliss <ldemmer6@gmail.com>
2019-01-29 16:10:51 +0100

    update travis.yml

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -34,9 +34,3 @@ echo "Attempting to build $project for OS X"
 
 echo 'Logs from build'
 cat $(pwd)/Logs/unity.log
-
-
-echo 'Attempting to zip builds'
-zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
-zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
-zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
