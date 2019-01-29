95d12b577f0ce4aefc438de1dc445374bf7354ca
Haeliss <ldemmer6@gmail.com>
2019-01-29 15:33:49 +0100

    update script build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -13,13 +13,13 @@ project="break-it"
 #   -quit
 
 echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \ 
+  -logFile $(pwd)/unity.log \ 
+  -projectPath $(pwd) \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
 # echo "Attempting to build $project for Linux"
