7a6c6cd719f2be58d9ea6a70f5eda9aa6f99c911
Haeliss <ldemmer6@gmail.com>
2019-02-05 17:28:30 +0100

    update for license

--- travis-scripts/before-script.sh
+++ travis-scripts/before-script.sh
@@ -3,6 +3,7 @@
 set -e
 set -x
 mkdir -p "/Library/Application Support/Unity/Unity_v2018.x.ulf"
+chmod a+x "/Library/Application Support/Unity/Unity_v2018.x.ulf"
 set +x
 echo 'Writing $UNITY_LICENSE_CONTENT to license file /Library/Application Support/Unity/Unity_v2018.x.ulf'
 echo "$UNITY_LICENSE_CONTENT" | tr -d '\r' > /Library/Application Support/Unity/Unity_v2018.x.ulf
