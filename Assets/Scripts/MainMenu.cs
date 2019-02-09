b6da220ef0b53fdf3f75e8301aa7c3cbc78db206
NathanFOURNIER <nathan.fournier13@gmail.com>
2019-02-09 20:06:43 +0100

    ajout des menus

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace jumpAndLearn.menus
+{
+    public class MainMenu : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        public void PlayGame()
+        {
+            SceneManager.LoadSceneAsync("Game");
+        }
+
+        public void Niveaux()
+        {
+            SceneManager.LoadSceneAsync("Niveaux");
+        }
+
+        public void Options()
+        {
+            SceneManager.LoadSceneAsync("Options");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
